74e8a9bf11adb3f5186f5e69359212dc743b9015
James Trask <jbt_5@yahoo.com>
2011-06-06 19:41:23 -0700

    added files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flow::Application.config.secret_token = '23a4e960fc7670822a7ec1fdc6cdbeeec93d6027174dd0f05eee918a84ca72e6e11a1393ba8e7b1ad212ef1c0b0931188ba8a3c29a079c15d9c44105b4344e64'
